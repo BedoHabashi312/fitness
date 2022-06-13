87af3bd5b8c6a67c15192a98bb052a83675dbc4d
BedoHabashi312 <BedoHabashi312>
2022-06-13 11:26:13 +0200

    main

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
